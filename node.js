e71549a2aee5d33ed3545f2d69097a48e39356cd
Vansh Saxena <102627940+vanshisop@users.noreply.github.com>
2024-09-26 05:15:25 +0530

    Update node.js

--- node.js
+++ node.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const { user } = require('pg/lib/defaults');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const { Pool } = require('pg');
 
 const pool = new Pool({
