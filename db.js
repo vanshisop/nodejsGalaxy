ff1aea18e9d0fa6a3fb4fafc576f935bdd696347
Vansh Saxena <102627940+vanshisop@users.noreply.github.com>
2024-09-26 05:19:22 +0530

    Delete db.js

--- db.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString: 'postgres://postgres.wewkjwtalqhjugzwrcql:_CPGJypV9RU$Nq-@aws-0-us-west-1.pooler.supabase.com:6543/postgres',
-});     
-
-
-module.exports = pool; 
